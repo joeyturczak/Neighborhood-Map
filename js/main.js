1b0a9e37b70185e6a5fb6cc2520766ae4596c74c
Joey Turczak <joeyturczak@gmail.com>
2017-12-10 00:15:09 -0800

    Add place list with dummy data

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ function initMap() {
     mapTypeControl: false
   });
 }
+
+var dummyItems = [
+  {
+    name: 'Place1'
+  },
+  {
+    name: 'Place2'
+  },
+  {
+    name: 'Place3'
+  },
+  {
+    name: 'Place4'
+  },
+  {
+    name: 'Place5'
+  }
+]
+
+var ViewModel = function() {
+  var self = this;
+
+  this.placeList = ko.observableArray([]);
+
+  dummyItems.forEach(function(place) {
+    self.placeList.push(new Place(place));
+  });
+}
+
+var Place = function(data) {
+  this.name = ko.observable(data.name);
+}
+
+ko.applyBindings(new ViewModel());
